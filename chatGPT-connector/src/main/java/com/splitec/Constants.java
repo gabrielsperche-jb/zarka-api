b574a777b80cd1e649f6efe7f3d0b3e4f5a6aea2
Gabriel Gonçalves <gabriel.goncalves@jitterbit.com>
2023-05-28 00:12:38 -0300

    ZBE-109 | Zarka API | Fixing GPT Key

--- chatGPT-connector/src/main/java/com/splitec/Constants.java
+++ chatGPT-connector/src/main/java/com/splitec/Constants.java
@@ -1,7 +1,7 @@
 package com.splitec;
 
 public interface Constants {
-  String TOKEN = "Bearer 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";
+  String TOKEN = "Bearer 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";
   String TOPIC = "Crie um resumo em tópicos do seguinte conteúdo: ";
   String STUDY = "Resuma o seguinte texto para estudo: ";
   String STUDY_TYPE = "study";
